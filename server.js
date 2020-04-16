5319f9b584440dc4591be9dbdc1f50ad5f9e9a82
Souriya Phongsavanh <souriya.phongsavanh777@gmail.com>
2020-04-16 16:05:58 +0200

    simplify code

--- server.js
+++ server.js
@@ -1,106 +1,24 @@
-const Vue = require('vue')
-const server = require('express')()
+const Vue = require("vue")
+const server = require("express")()
 
-const createApp = require('./factory')
-
-const singletonInstance = new Vue({
+const SINGLETON_INSTANCE = new Vue({
   data: {
-    randomNb: Math.random()
+    randomNb: Math.random(),
   },
-  template: `<div>singleton random number: {{ randomNb }}</div>`
+  template: `<div>"cached" random number: {{ randomNb }}</div>`,
 })
 
-server.get('/with-template', (req, res) => {
-  const myVueInstance = new Vue({
+server.get(['/', '/ssr-inline-html'], (req, res) => {
+  const vm = new Vue({
     data: {
-      url: req.url
+      url: req.url,
     },
-    template: `<div>😎 WITH template {{ url }}</div>`
+    template: `<div>url: {{ url }}</div>`,
   })
 
-  const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./index.template.html', 'utf-8')
-  })
+  const renderer = require("vue-server-renderer").createRenderer()
 
-  renderer.renderToString(myVueInstance, (_, html) => {
-    res.end(html)
-  })
-})
-
-server.get('/template-interpolation', (_, res) => {
-  const subComponent = Vue.component('sub-component', {
-    data: function() {
-      return {
-        subCount: 888
-      }
-    },
-    template: `<div>my sub-count: {{ subCount }}</div>`
-  })
-  const app = new Vue({
-    data: {
-      count: 777,
-      randomNb: Math.random()
-    },
-    components: { subComponent },
-    template: `<div>
-      my count: {{ count }} random {{ randomNb }}
-      <sub-component />
-    </div>`
-  })
-
-  const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./template-interpolation.html', 'utf-8')
-  })
-
-  const context = {
-    title: 'my beautiful title 🔥',
-    meta: `
-      <meta name="description" content="My description">
-    `,
-    msg: `Le bonheur n'est réel que s'il est partagé.`
-  }
-
-  renderer.renderToString(app, context, (_, html) => {
-    res.end(html)
-  })
-})
-
-server.get('/singleton-instance', (_, res) => {
-  const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./index.template.html', 'utf-8')
-  })
-
-  renderer.renderToString(singletonInstance, (_, html) => {
-    res.end(html)
-  })
-})
-
-server.get('/factory', (req, res) => {
-  const context = { url: req.url }
-  const app = createApp(context)
-
-  const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./index.template.html', 'utf-8')
-  })
-  renderer.renderToString(app, (_, html) => {
-    res.end(html)
-  })
-})
-
-server.get('*', (req, res) => {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: `<div>🙏 The visited URL is {{ url }}</div>`
-  })
-
-  const renderer = require('vue-server-renderer').createRenderer()
-
-  renderer.renderToString(app, (err, html) => {
-    if (err) {
-      res.status(500).end('Internal Server Error')
-    }
+  renderer.renderToString(vm, (_, html) => {
     res.end(`
       <!DOCTYPE html>
       <html lang="en">
@@ -111,4 +29,56 @@ server.get('*', (req, res) => {
   })
 })
 
-server.listen(8080)
+server.get("/ssr-template-html", (_, res) => {
+  const subComponent = Vue.component("sub-component", {
+    data: function () {
+      return {
+        subCount: 222,
+      }
+    },
+    template: `<div>my sub-count: {{ subCount }}</div>`,
+  })
+  const app = new Vue({
+    data: {
+      count: 111,
+      randomNb: Math.random(),
+    },
+    components: { subComponent },
+    template: `<div>
+      my count: {{ count }} random {{ randomNb }}
+      <sub-component />
+    </div>`,
+  })
+
+  const renderer = require("vue-server-renderer").createRenderer({
+    template: require("fs").readFileSync(
+      "./template/template-interpolation.html",
+      "utf-8"
+    ),
+  })
+
+  const context = {
+    title: "my beautiful title 🔥",
+    meta: `
+      <meta name="description" content="My description">
+    `,
+    msg: `Le bonheur n'est réel que s'il est partagé.`,
+  }
+
+  renderer.renderToString(app, context, (_, html) => {
+    res.end(html)
+  })
+})
+
+server.get("/ssr-with-cache", (_, res) => {
+  const renderer = require("vue-server-renderer").createRenderer({
+    template: require("fs").readFileSync("./template/index.html", "utf-8"),
+  })
+
+  renderer.renderToString(SINGLETON_INSTANCE, (_, html) => {
+    res.end(html)
+  })
+})
+
+console.log(`Listen on port 3000`)
+server.listen(3000)
